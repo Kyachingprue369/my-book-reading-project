ec57aaa1849fddfe1673287ff220e3c5b42e7aa3
kyachingprue <kyachingpruemarma.info@gmail.com>
2025-02-26 23:35:00 +0600

    new commit

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <div>
+      <h2 className='text-center py-7 text-4xl font-bold'>Hello Main Components</h2>
+    </div>
+  );
+};
+
+export default Main;
